02e3e0f5685b8cf50520238081b1fb360d27690f
grandbora <bora.tunca@yahoo.com>
2012-12-08 13:26:42 +0100

    asset temp config fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Repo::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.compile = true #BDNF
 
   # Generate digests for assets URLs
   config.assets.digest = true
