27127288c5515e5b6b0dac6cc0a63398c50056c4
grandbora <bora.tunca@yahoo.com>
2013-02-24 14:00:50 +0100

    jq watermark removed css changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,5 @@
-//= require_tree .
 //= require jquery.min
 //= require jquery-ui.min
-//= require jquery.watermark.min
 //= require googleanalytics
 //= require uservoice
+//= require_tree .
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,14 +6,14 @@ var searchTimeoutId;
  * @param q search parameter
  */
 function observeSearchValue(q) {
-	if (lastQ === q) return;
-	lastQ = q;
+  if (lastQ === q) return;
+  lastQ = q;
 
-	clearTimeout(searchTimeoutId);
+  clearTimeout(searchTimeoutId);
 
-	searchTimeoutId = setTimeout(function(){
-		filterQuotes(q);
-	}, 1000);
+  searchTimeoutId = setTimeout(function(){
+    filterQuotes(q);
+  }, 1000);
 }
 
 /*
@@ -21,18 +21,53 @@ function observeSearchValue(q) {
  * @param q search parameter
  */
 function filterQuotes(q) {
-	resetQuotes();
+  resetQuotes();
 
-	if ('' == q) return ;
+  if ('' == q) return ;
 
-	$(".list ul li:not(:contains('"+q+"'))").hide();
+  $(".list ul li:not(:contains('"+q+"'))").hide();
 
-	_gaq.push(['_trackEvent', 'interaction', 'Search', q]);
+  _gaq.push(['_trackEvent', 'interaction', 'Search', q]);
 }
 
 /*
  *
  */
 function resetQuotes() {
-	$(".list ul li").show();
+  $(".list ul li").show();
 }
+
+$(".book").autocomplete({
+  source: function(request, response) {
+    $.ajax({
+      url: "http://ws.geonames.org/searchJSON",
+      dataType: "jsonp",
+      data: {
+        featureClass: "P",
+        style: "full",
+        maxRows: 12,
+        name_startsWith: request.term
+      },
+      success: function( data ) {
+        response( $.map( data.geonames, function( item ) {
+          return {
+            label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
+            value: item.name
+          }
+        }));
+      }
+    });
+  },
+  minLength: 2,
+  select: function( event, ui ) {
+    log( ui.item ?
+      "Selected: " + ui.item.label :
+      "Nothing selected, input was " + this.value);
+  },
+  open: function() {
+    $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+  },
+  close: function() {
+    $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+  }
+});
