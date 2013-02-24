96fb850f9557708638f044b2bf90ab69134935c7
grandbora <bora.tunca@yahoo.com>
2013-02-24 13:20:32 +0100

    add new boxes altered

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
-	
-	def index
-		@quotes = Quote.all(:order => "created_at DESC", :limit => 150)
-	end
+  
+  def index
+    @quotes = Quote.all(:order => "created_at DESC", :limit => 150)
+  end
 
-	def save
-		@quote = Quote.new(params['quote'])
-		@quote.save
-		return redirect_to :action => "index"
-	end
+  def save
+    @quote = Quote.new(params['quote'])
+    @quote.save
+    return redirect_to :action => "index"
+  end
 end
