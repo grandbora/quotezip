f00de3ceeeab2bd3ca57e6bdf4cd30804447affe
grandbora <bora.tunca@yahoo.com>
2013-03-04 00:49:19 +0100

    mail conf test

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,12 @@ require File.expand_path('../application', __FILE__)
 QuotezipApp::Application.initialize!
 
 # Sendgrid config
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'quotezip.herokuapp.com' #TODO: get rid of hardcoded domain
-}
-ActionMailer::Base.delivery_method ||= :smtp
+# ActionMailer::Base.smtp_settings = {
+#   :address        => 'smtp.sendgrid.net',
+#   :port           => '587',
+#   :authentication => :plain,
+#   :user_name      => ENV['SENDGRID_USERNAME'],
+#   :password       => ENV['SENDGRID_PASSWORD'],
+#   :domain         => 'quotezip.herokuapp.com' #TODO: get rid of hardcoded domain
+# }
+# ActionMailer::Base.delivery_method ||= :smtp
