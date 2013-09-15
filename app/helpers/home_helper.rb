0c1f7bc26da9f64d1e31c7f03ebbaecaea1a94c0
grandbora <bora.tunca@yahoo.com>
2013-09-16 01:54:40 +0200

    meta meta

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,7 +4,9 @@ module HomeHelper
 
   def social_share_button_content(content)
 
-    offsetLength = @extra_meta.length + 2 + request.original_url.length #TODO : 2 is for the space and colon !! make a pr
+    offsetLength = @extra_meta.length + 2
+    offsetLength =+ request.original_url.length #TODO : 2 is for the space and colon !! make a pr
+    
     maxLength = 140 - offsetLength
 
     return content + @extra_meta if content.length < maxLength
