98d6c3b6e72c1deaf2a237c243da2bf112ec6246
grandbora <bora.tunca@yahoo.com>
2013-09-16 02:42:15 +0200

    meta meta

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,7 +2,7 @@ module HomeHelper
 
   def social_share_description(content)
     extra_meta = "... #quotezip "
-    offsetLength = extra_meta.length + 2 + request.original_url.length #TODO : 2 is for the space and colon !! make a pr
+    offsetLength = extra_meta.length + 2 + get_url.length #TODO : 2 is for the space and colon !! make a pr
     maxLength = 140 - offsetLength
 
     result = content + extra_meta
@@ -11,7 +11,12 @@ module HomeHelper
 
   def social_share_button_tag(quote)
     content = social_share_description quote.content
-    url = request.protocol + request.host + "/" + quote.id.to_s
+    url = get_url
     super(content, {url: url})
+  end
+
+  private
+  def get_url
+    request.protocol + request.host + "/" + quote.id.to_s
   end
 end
