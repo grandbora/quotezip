842eb945ab1f405493c2de3440ce059386a6d7b5
grandbora <bora.tunca@yahoo.com>
2013-11-17 20:11:59 +0100

    init rails 4

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.1'
+gem 'mysql2'
+gem 'unicorn'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuoteApp::Application.config.session_store :cookie_store, key: '_quoteApp_session'
