bbf1bac5caa57214177539f9c9c27332fa27def5
grandbora <bora.tunca@yahoo.com>
2013-12-01 21:31:44 +0100

    remove reported from feed

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,6 +5,8 @@ class Quote < ActiveRecord::Base
   scope :feed,
     select('quotes.*, count(votes.id) as votes_count').
     joins('left join votes on quotes.id = votes.quote_id').
+    joins('left join reports on quotes.id = reports.quote_id').
+    where('reports.id is NULL').
     group('quotes.id').
     order('votes_count DESC, quotes.updated_at DESC')
 
