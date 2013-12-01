d96b93891dc04fe018e179059d661011477241e6
grandbora <bora.tunca@yahoo.com>
2013-12-01 19:34:58 +0100

    single single single

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -8,4 +8,12 @@ class FeedController < ApplicationController
     @quotes = Quote.feed
     render "index"
   end
+
+  def show
+    quoteRelation = Quote.where("id = :id", {:id => params['id']})
+    render :status => 404 if quoteRelation.empty?
+
+    @quote = quoteRelation.first
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 QuoteApp::Application.routes.draw do
   root to: "feed#index"
   get "/uncached", to: "feed#uncached"
+  get ":id" => "feed#show", :constraints => { :id => /[0-9]*/ }
+
   resources :votes
   resources :reports
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
